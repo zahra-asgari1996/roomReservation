420d328a8fc1ecef3234f1f72f4e2a3b05bb5491
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-03-02 12:34:18 +0330

    create form

---
+++ src/main/java/form.java
@@ -0,0 +1,39 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "form")
+public class form extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String req = request.getParameter("option");
+
+        if(req.equalsIgnoreCase("reserve")){
+            RequestDispatcher rd=request.getRequestDispatcher("form.html");
+            rd.forward(request,response);
+
+        }else if(req.equalsIgnoreCase("show")){
+            RequestDispatcher rd=request.getRequestDispatcher("nationalCode.html");
+            rd.forward(request,response);
+
+
+        }else if(req.equalsIgnoreCase("cancel")){
+            RequestDispatcher rd=request.getRequestDispatcher("reserveCode.html");
+            rd.forward(request,response);
+
+
+        }else {
+            RequestDispatcher rd=request.getRequestDispatcher("reserveCode.html");
+            rd.forward(request,response);
+
+        }
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Welcome to Room Reservation System</h2>
+
+
+<form method="get" action="form">
+    <input type="radio" id="reserve" name="option" value="reserve">
+    <label for="reserve">Reserve</label><br>
+    <input type="radio" id="show" name="option" value="show">
+    <label for="show">Show</label><br>
+    <input type="radio" id="change" name="option" value="change">
+    <label for="change">Change</label><br>
+    <input type="radio" id="cancel" name="option" value="cancel">
+    <label for="cancel">Cancel</label><br><br>
+    <input type="submit" value="login"/><br>
+</form>
+
+</body>
+</html>
