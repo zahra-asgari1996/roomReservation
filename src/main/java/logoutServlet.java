1e0b60da063dace50b58ebb97c406b3e0429a69a
zahra.asgari <zahra.asgari1996@yahoo.com>
2021-03-12 20:52:51 +0330

    completing room reservation system

--- src/main/java/logoutServlet.java
+++ src/main/java/logoutServlet.java
@@ -3,22 +3,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "showInformationServlet")
-public class showInformationServlet extends HttpServlet {
+@WebServlet(name = "logoutServlet")
+public class logoutServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        RoomReservationDao roomReservationDao=new RoomReservationDao();
-        out.println(roomReservationDao.showInformation(Integer.parseInt(request.getParameter("nationalCode"))));
-
-
-
-
+        HttpSession session= request.getSession(false);
+        if(session!=null){
+            session.invalidate();
+            out.println("You Log out Successfully ");
+        }else{
+            out.println("You Log out Before ");
+        }
     }
 }
